50f63d53a90325abfbef6337e608417889167330
MADALA LEELA VENKATA SIVA SPOORTHI <mmadalavenkatesh@gmail.com>
2025-06-27 12:20:22 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true); 
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`✅ MongoDB Connected: ${conn.connection.host} in ${process.env.NODE_ENV} mode`);
+        console.log(`MongoDB Connected: ${conn.connection.host} in ${process.env.NODE_ENV} mode`);
     } catch (err) {
-        console.error(`❌ MongoDB Connection Error: ${err.message}`);
+        console.error(`MongoDB Connection Error: ${err.message}`);
         process.exit(1);
     }
 };
